ddcfd2b50de262d03886e59da8ccfa023c98f8f4
Kareem fady <113119129+kemofady@users.noreply.github.com>
2023-10-04 12:32:55 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+
+/* library **/
+#include<util/delay.h>
+#include"STD_TYPES.h"
+#include"BIT_MATH.h"
+/* mcal */
+
+#include"DIO_driver/Dio_Interface.h"
+
+/* own  drive*/
+
+#include"LCD_driver/LCD_interface.h"
+ //--------------------------------------
+
+void main(){
+	LCD_voidinit();
+
+
+	while(1){
+		// Assignment LCD name in sinusoidal
+		LCD_voidnameinsinusoidal();
+
+
+			}
+
+	}
+
